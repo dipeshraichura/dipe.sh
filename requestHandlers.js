db7f6e1eff0a33b0eb54b6836683f17ffd554eab
drkelkoo <dipesh.raichura@yahoo-inc.com>
2015-09-07 19:07:59 -0700

    added basics

--- requestHandlers.js
+++ requestHandlers.js
@@ -4,7 +4,7 @@ var http = require('http');
 function homepage(response) {
 		console.log("Request handler 'upload' was called.");
 		response.writeHead(200, {"Content-Type": "text/html"}); 
-		response.write("Hello World");
+		response.write("dipesh raichura");
 		response.end();
 }	
 
